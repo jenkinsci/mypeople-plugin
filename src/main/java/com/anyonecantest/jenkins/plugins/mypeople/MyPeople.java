934fdbdd1d353a5b0ae827c868225e6726904e27
anyone-can-test <anyone.can.test@gmail.com>
2013-12-20 14:01:07 +0900

    first commit

---
+++ src/main/java/com/anyonecantest/jenkins/plugins/mypeople/MyPeople.java
@@ -0,0 +1,122 @@
+package com.anyonecantest.jenkins.plugins.mypeople;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+
+import net.sf.json.JSONObject;
+
+import org.apache.http.HttpHost;
+import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.params.HttpParams;
+import org.apache.http.params.BasicHttpParams;
+import org.apache.http.client.HttpClient;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.conn.params.ConnRoutePNames;
+import org.apache.http.util.EntityUtils;
+import org.apache.http.HttpResponse;
+import org.apache.http.entity.mime.MultipartEntity;
+import org.apache.http.entity.mime.content.StringBody;
+
+import java.util.logging.Logger;
+
+import com.anyonecantest.jenkins.plugins.mypeople.model.BuildState;
+
+
+
+/**
+ * @author Hyunil Shin
+ */
+public class MyPeople {
+	
+	// My People Open API
+	private final static String RESULT_CODE_NAME = "code";
+	private final static String RESULT_MESSAGE_NAME = "message";
+	private final static int RESULT_OK = 200;
+
+	private static String API_URL_PREFIX = "https://apis.daum.net";
+	private static String MYPEOPLE_BOT_APIKEY;
+	private static String API_URL_POSTFIX;
+	
+	
+	private final static Logger LOG = Logger.getLogger("mypeople");
+	
+	
+	private MyPeople() {
+
+	}
+	
+	public static void SetAPIKEY(String apikey) {
+		MYPEOPLE_BOT_APIKEY = apikey;
+		API_URL_POSTFIX = "?apikey=" + MYPEOPLE_BOT_APIKEY;
+	}
+
+	
+	public static String getMessage(BuildState buildState) {
+		
+		String msg;
+		msg = "Build Failure!!!";
+		msg += "    " + buildState.getFullUrl();
+		
+		return msg;
+	}
+	
+ 
+	public static boolean sendMessage(String buddyId, String msg) throws ClientProtocolException, IOException, UnsupportedEncodingException
+        {
+		String requestUrl = MyPeople.API_URL_PREFIX + "/mypeople/buddy/send.json";
+        requestUrl += API_URL_POSTFIX;
+   
+	
+		MultipartEntity reqEntity = new MultipartEntity();
+
+		StringBody sbody = new StringBody(buddyId);
+		reqEntity.addPart("buddyId", sbody);
+
+		sbody = new StringBody(msg);
+		reqEntity.addPart("content", sbody);
+
+
+   		return postData(requestUrl, reqEntity); 
+
+	}
+
+
+
+	public static boolean postData(String url, MultipartEntity reqEntity) throws ClientProtocolException, IOException {
+
+		LOG.info("url: " + url);
+
+		HttpParams myParams = new BasicHttpParams();
+		HttpConnectionParams.setConnectionTimeout(myParams, 10000);
+		HttpConnectionParams.setSoTimeout(myParams, 10000);
+		
+		HttpClient httpclient = new DefaultHttpClient(myParams);
+
+		String proxyPort = System.getProperty("http.proxyPort");
+		String proxyHost = System.getProperty("http.proxyHost");
+		
+
+		if(proxyHost != null) {
+			httpclient.getParams().setParameter(ConnRoutePNames.DEFAULT_PROXY, new HttpHost(proxyHost, Integer.parseInt(proxyPort), "http"));
+		}
+				
+		HttpPost httppost = new HttpPost(url.toString());
+		httppost.setEntity(reqEntity);
+		HttpResponse response = httpclient.execute(httppost);
+		
+		String result = EntityUtils.toString(response.getEntity());
+		LOG.info("response: " + result);
+		
+		JSONObject jo = JSONObject.fromObject(result);
+		int resultCode = Integer.valueOf((String)jo.get(MyPeople.RESULT_CODE_NAME));
+		if(resultCode == MyPeople.RESULT_OK) {
+			return true;
+		}
+		
+		return false;						
+	}
+
+}
---
+++ src/main/java/com/anyonecantest/jenkins/plugins/mypeople/Phase.java
@@ -0,0 +1,103 @@
+/**
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.anyonecantest.jenkins.plugins.mypeople;
+
+import hudson.EnvVars;
+
+import hudson.model.AbstractBuild;
+import hudson.model.Hudson;
+import hudson.model.Job;
+import hudson.model.ParameterValue;
+import hudson.model.ParametersAction;
+import hudson.model.Result;
+import hudson.model.Run;
+import hudson.model.TaskListener;
+
+
+import java.util.List;
+
+import com.anyonecantest.jenkins.plugins.mypeople.model.BuildState;
+import com.anyonecantest.jenkins.plugins.mypeople.model.JobState;
+
+@SuppressWarnings({ "unchecked", "rawtypes" })
+public enum Phase {
+	STARTED, COMPLETED, FINISHED;
+
+	public void handle(Run run, TaskListener listener) {
+		HudsonNotificationProperty property = (HudsonNotificationProperty) run.getParent().getProperty(HudsonNotificationProperty.class);
+		if (property != null) {
+			
+			JobState jobState = buildJobState(run.getParent(), run);
+			List<Endpoint> targets = property.getEndpoints();
+			for (Endpoint target : targets) {
+          		try {
+               					                       		               		               		
+               		if(jobState.getBuild().isFailure() == true) {
+               			
+               			String msg = MyPeople.getMessage(jobState.getBuild());
+               			MyPeople.SetAPIKEY(target.getApikey());
+               			boolean success = MyPeople.sendMessage(target.getBuddyId(), msg);
+               			               			
+               		}
+			        
+           		} catch (Exception e) {
+     
+          			e.printStackTrace(listener.error("Failed to notify "+ target));
+           		}
+       		}
+         
+            
+		}
+
+	}
+	
+	private JobState buildJobState(Job job, Run run) {
+		JobState jobState = new JobState();
+		jobState.setName(job.getName());
+		jobState.setUrl(job.getUrl());
+		BuildState buildState = new BuildState();
+		buildState.setNumber(run.number);
+		buildState.setUrl(run.getUrl());
+		buildState.setPhase(this);
+		buildState.setStatus(getStatus(run));
+
+		String rootUrl = Hudson.getInstance().getRootUrl();
+		if (rootUrl != null) {
+			buildState.setFullUrl(rootUrl + run.getUrl());
+		}
+
+		jobState.setBuild(buildState);
+
+		ParametersAction paramsAction = run.getAction(ParametersAction.class);
+		if (paramsAction != null && run instanceof AbstractBuild) {
+			AbstractBuild build = (AbstractBuild) run;
+			EnvVars env = new EnvVars();
+			for (ParameterValue value : paramsAction.getParameters())
+				if (!value.isSensitive())
+					value.buildEnvVars(build, env);
+			buildState.setParameters(env);
+		}
+		
+		return jobState;
+	}
+	
+	private String getStatus(Run r) {
+		Result result = r.getResult();
+		String status = null;
+		if (result != null) {
+			status = result.toString();
+		}
+		return status;
+	}
+}
